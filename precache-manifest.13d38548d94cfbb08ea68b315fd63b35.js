e5921b3a825a5f2d58b2f90b54ad62ea665ea6ff
stevematdavies <stevematdavies@gmail.com>
2020-09-23 19:13:16 +0300

    Updates

---
+++ precache-manifest.13d38548d94cfbb08ea68b315fd63b35.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "024d69142f5ff8de0c13c4e98488da5c",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "784a78d5d470c0fabbac",
+    "url": "/monsters-rolodex/static/css/main.d2537039.chunk.css"
+  },
+  {
+    "revision": "89412ccff4c48751ad1a",
+    "url": "/monsters-rolodex/static/js/2.0a4e41ba.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.0a4e41ba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "784a78d5d470c0fabbac",
+    "url": "/monsters-rolodex/static/js/main.eb83ed00.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
